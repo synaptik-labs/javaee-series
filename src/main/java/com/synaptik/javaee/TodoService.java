9d3b4abb80f8e6597ffda14cd3c73cbfeb549630
Dan Watling <dan@synaptik.com>
2017-12-04 06:55:56 -0600

    Episode 2 - API Definition

--- src/main/java/com/synaptik/javaee/TodoService.java
+++ src/main/java/com/synaptik/javaee/TodoService.java
@@ -1,12 +1,34 @@
 package com.synaptik.javaee;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import com.synaptik.javaee.data.Todo;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/")
 public class TodoService {
 	@GET
-	public String get() {
-		return "Hello Jimmy!";
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Todo> getAll() {
+		return null;
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void create(Todo incoming) {
+
+	}
+
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void update(@PathParam("id") Long id, Todo incoming) {
+
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void remove(@PathParam("id") Long id) {
 	}
 }
---
+++ src/main/java/com/synaptik/javaee/data/Todo.java
@@ -0,0 +1,22 @@
+package com.synaptik.javaee.data;
+
+public class Todo {
+	private Long id;
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
