d060dfc973702477a993ae7c0664074eec11816b
Dan Watling <dan@synaptik.com>
2017-12-20 10:20:26 -0600

    Episode 5 - Tie EJB into JAX-RS

--- src/main/java/com/synaptik/javaee/TodoService.java
+++ src/main/java/com/synaptik/javaee/TodoService.java
@@ -2,33 +2,38 @@ package com.synaptik.javaee;
 
 import com.synaptik.javaee.data.Todo;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/")
 public class TodoService {
+	@Inject
+	TodoEJB ejb;
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Todo> getAll() {
-		return null;
+		return ejb.findAll();
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void create(Todo incoming) {
-
+		ejb.create(incoming);
 	}
 
 	@PUT
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void update(@PathParam("id") Long id, Todo incoming) {
-
+		ejb.update(id, incoming);
 	}
 
 	@DELETE
 	@Path("{id}")
 	public void remove(@PathParam("id") Long id) {
+		ejb.remove(id);
 	}
 }
